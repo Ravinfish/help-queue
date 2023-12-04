4c37d5ce6b8f3106aec73a847dea8da9c0a791f5
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-04 08:54:21 -0800

    add to header

---
+++ src/componants/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ticketsImage from "./../img"
+
+function Header(){
+  return (
+    <h1>Help Queue</h1>
+    <img src={ticketsImage} alt="An image of tickets" />
+  );
+}
+
+export default Header;
